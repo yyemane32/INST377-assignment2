b5bb0ce939c65258ee2ed562fc6ff7f43e9c4fd4
yyemane32 <yohanayemane57@gmail.com>
2020-11-09 20:08:58 -0500

    server change

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 /* eslint-disable no-unused-vars */
 import express from 'express';
 import dotenv from 'dotenv';
-import countries from"./public/lab_6/countries.js"
+import fetch from 'node-fetch';
 
 dotenv.config();
 
